13e1974476d0b14ba67ca5088ac4c1ad1f9904bd
Martin Michel <dev@famic.de>
2017-11-21 21:52:32 +0100

    Copyright and licence notice added

--- def.scm
+++ def.scm
@@ -1,4 +1,22 @@
-;;; Customized model definition 
+;;; def.scm
+;;; Definition of model and boundary conditions in Thermograph's DSL
+;;; 
+;;; This file is part of Thermograph.
+;;; 
+;;; Thermograph is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;; 
+;;; Thermograph is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;; 
+;;; You should have received a copy of the GNU General Public License
+;;; along with Thermograph. If not, see <http://www.gnu.org/licenses/>.
+;;; 
+;;; Copyright 2016 Martin Michel
 
 (valve ws (in 1) (out 2) (param 0.99)) 
 (header (in '(2 3)) (out '(4 5)))
@@ -10,5 +28,7 @@
 
 (set! eqns '())
 (set! J '())
+
+; the following repetiton is still required to …
 (valve ws (in 1) (out 2) (param 0.99)) 
 (header (in '(2 3)) (out '(4 5)))
--- scripts/bcs.scm
+++ scripts/bcs.scm
@@ -1,3 +1,24 @@
+;;; bcs.scm
+;;; Extract equations for boundary conditions according model
+;;; definition.
+;;; 
+;;; This file is part of Thermograph.
+;;; 
+;;; Thermograph is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;; 
+;;; Thermograph is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;; 
+;;; You should have received a copy of the GNU General Public License
+;;; along with Thermograph. If not, see <http://www.gnu.org/licenses/>.
+;;; 
+;;; Copyright 2016 Martin Michel
+
 ;;; get string for parameter node
 (define paramptr
   (lambda (x node)
@@ -37,14 +58,9 @@
                   "{{df}}[" (number->string (+ 2 (x-get node))) "]=+1;")]))
         (cdr blist)))))))
 
-
-
-
 ;;; Macro for injecting boundary conditions into system
 (define-syntax provide-params
   (syntax-rules ()
         ((_ (node var* ...)* ...)
          (begin
            (for-each extract (list (node var* ...)* ... ))))))
-
-
